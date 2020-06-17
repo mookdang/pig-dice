2d154745320434fa3261d50a5e6d82f8d37e9479
mook <mookdang24@gmail.com>
2020-06-17 15:11:31 -0700

    we are on js fiddle trying to undertand how to make it works.
    Co-authored by Tristan Emmerson<tristan@stickerslug.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,30 +1,31 @@
 //Business Logic for player
-function Player(name, runningTotal, active, diceRoll, turnCount) {
+function Player(name, scoreTotal, roundTotal, isActive, diceRoll) {
   this.name = name;
-  this.runningTotal = 0;
-  this.active = active;
+  this.scoreTotal = 0;
+  this.roundTotal = 0;
+  this.isActive = isActive;
   this.diceRoll = 0;
-  this.turnCount = 0;
-
+}
 
 Player.prototype.Roll = function () {
-  let randomRoll = Math.floor(Math.random() * 6) + 1);//random between 0-6
+  let randomRoll = Math.floor((Math.random() * 6) + 1); //random between 0-6
   this.diceRoll = randomRoll;
   if (randomRoll === 1) {
-    this.turnCount = 0;
+    this.roundTotal = 0;
     this.diceRoll = 1;
-    console.log("Next Player");
-    console.log(diceRoll);
-  }else{
-    console.log("Keep rolling");
-    console.log(diceRoll);
+    alert("you rolled 1");
+    console.log("you rolled 1")
+
+    console.log("you rolled something else")
   }
+  return this.diceRoll
 } 
 
 
 
-let dice1=(Math.random() * 6) + 1);
-let dice2=(Math.random() * 6) + 1);
+
+let dice1= Math.floor((Math.random() * 6) + 1);
+let dice2= Math.floor((Math.random() * 6) + 1);
 let sum = dice1 + dice2;
 
 
@@ -32,3 +33,4 @@ let sum = dice1 + dice2;
 
 let btnRoll = document.querySelector('.btn-roll');
 let btnHold = document.querySelector('.btn-hold');
+
